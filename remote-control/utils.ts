08943b20880f491479db8e9f18523ec2c72196e7
XieJiSS <XieJiSS@users.noreply.github.com>
2022-12-04 22:03:52 +0800

    fix: try to fix startHbbServer

--- remote-control/utils.ts
+++ remote-control/utils.ts
@@ -4,8 +4,9 @@ import cp from "child_process";
 import path from "path";
 import { setTimeout as sleep } from "timers/promises";
 import { promisify } from "util";
+import type { TPromisify } from "../types/type-helper";
 
-const spawn = promisify(cp.spawn);
+const spawn: TPromisify<typeof cp.spawn> = promisify(cp.spawn);
 
 const HOST_IP = "127.0.0.1";
 
@@ -15,8 +16,8 @@ export async function startHbbServer() {
     stdio: "ignore",
   });
   await sleep(1000);
-  await spawn(path.join(__dirname, "..", "thirdparty", "rustdesk", "hbbr.exe"), [], {
-    detached: true,
-    stdio: "ignore",
+  const proc = await spawn(path.join(__dirname, "..", "thirdparty", "rustdesk", "hbbr.exe"), [], {
+    windowsHide: true,
   });
+  proc.unref();
 }

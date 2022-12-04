887786a35b63f40948327e6075efee74931c9ce2
XieJiSS <XieJiSS@users.noreply.github.com>
2022-12-04 20:55:43 +0800

    fix: missing module

--- servers/lists.ts
+++ servers/lists.ts
@@ -1,5 +1,8 @@
+/** @format */
+
+// use relative path so that node.js require() can resolve paths correctly
 export default [
-  "remote-control/remote-control-server.js", // control remote clients
-  "camera/camera-server.js", // control camera
-  "panel/panel-server.js", // backend of panel page
+  "../remote-control/remote-control-server.js", // control remote clients
+  "../camera/camera-server.js", // control camera
+  "../panel/panel-server.js", // backend of panel page
 ];

3b5c77257340f752cfc21302e87a5dd78616ee25
XieJiSS <XieJiSS@users.noreply.github.com>
2022-12-04 22:10:26 +0800

    fix: child_process.spawn is not promisify-able

--- remote-control/utils.ts
+++ remote-control/utils.ts
@@ -3,21 +3,25 @@
 import cp from "child_process";
 import path from "path";
 import { setTimeout as sleep } from "timers/promises";
-import { promisify } from "util";
-import type { TPromisify } from "../types/type-helper";
-
-const spawn: TPromisify<typeof cp.spawn> = promisify(cp.spawn);
 
 const HOST_IP = "127.0.0.1";
 
 export async function startHbbServer() {
-  await spawn(path.join(__dirname, "..", "thirdparty", "rustdesk", "hbbs.exe"), ["-r", HOST_IP, "-R", HOST_IP], {
-    detached: true,
-    stdio: "ignore",
-  });
+  const hbbs = cp.spawn(
+    path.join(__dirname, "..", "thirdparty", "rustdesk", "hbbs.exe"),
+    ["-r", HOST_IP, "-R", HOST_IP],
+    {
+      windowsHide: true,
+      stdio: "ignore",
+      detached: true,
+    }
+  );
+  hbbs.unref();
   await sleep(1000);
-  const proc = await spawn(path.join(__dirname, "..", "thirdparty", "rustdesk", "hbbr.exe"), [], {
+  const hbbr = cp.spawn(path.join(__dirname, "..", "thirdparty", "rustdesk", "hbbr.exe"), [], {
     windowsHide: true,
+    stdio: "ignore",
+    detached: true,
   });
-  proc.unref();
+  hbbr.unref();
 }

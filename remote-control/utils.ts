7d3f817b16e4f0dbf0f1fee0e6f52fe360c51a7f
XieJiSS <XieJiSS@users.noreply.github.com>
2022-12-17 00:51:32 +0800

    fix: should only start hbbr/hbbs on windows

--- remote-control/utils.ts
+++ remote-control/utils.ts
@@ -1,12 +1,20 @@
 /** @format */
 
+import os from "os";
 import cp from "child_process";
 import path from "path";
 import { setTimeout as sleep } from "timers/promises";
 
+import getLogger from "../util/logger";
+const logger = getLogger("rustdesk-wrapper");
+
 const HOST_IP = "127.0.0.1";
 
 export async function startHbbServer() {
+  if (os.platform() !== "win32") {
+    logger.warn("Currently can't start rustdesk hbbs/hbbr server on non-Windows platforms.");
+    return;
+  }
   const hbbs = cp.spawn(
     path.join(__dirname, "..", "thirdparty", "rustdesk", "hbbs.exe"),
     ["-r", HOST_IP, "-R", HOST_IP],
